97075be1ebe33dcaaa15604024e70948252c20c1
Louis,Park <dntjd0313@gmail.com>
2015-08-27 15:04:56 +0900

    ListView 연습

--- app/src/main/java/com/android/example/listviewexam/MainActivity.java
+++ app/src/main/java/com/android/example/listviewexam/MainActivity.java
@@ -1,37 +1,53 @@
+
 package com.android.example.listviewexam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private List<String> mData;
+    private ArrayAdapter<String> mAdapter; // Adapter작성
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1.Data 준비
+        initData();
+
+        // 2. Adapter 준비
+        initAdapter();
+
+        // 3. ListView 에 Adapter 설정.
+        initListView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void initData() {
+        mData = new ArrayList<>();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        for (int i = 1; i <= 100; i++) {
+            mData.add("item " + i);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void initAdapter() {
+        // initAdapter() 메소드 작성
+        mAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, mData);
+    }
+
+    private void initListView() {
+        mListView = (ListView) findViewById(R.id.list_view);
+        mListView.setAdapter(mAdapter);
+    }
+
 }
